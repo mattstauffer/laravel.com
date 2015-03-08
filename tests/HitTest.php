fead02623196c0dda77e271e447c2c1f7eefe20d
Matt Stauffer <matt@tighten.co>
2015-03-08 09:53:01 -0400

    Extract stubs and add test for beginning ellipses

--- tests/HitTest.php
+++ tests/HitTest.php
@@ -4,29 +4,24 @@ use App\Services\Documentation\Hit;
 
 class HitTest extends TestCase {
 
-	private function stubHit()
+	private function stubPlain()
 	{
-		return [
-			'_id' => '1234512351235',
-			'_score' => 5,
-			'_source' => [
-				'title' => null,
-				'slug' => null,
-				'body.md' => null,
-				'body.highlighting' => null,
-				'body.html' => null
-			],
-			'highlight' => [
-				'body.highlighting' => [
-					'fragment <mark>here</mark> I think'
-				]
-			]
-		];
+		return include(__DIR__ . '/stubs/plainHit.php');
+	}
+
+	private function stubMailtrap()
+	{
+		return include(__DIR__ . '/stubs/mailtrapHit.php');
+	}
+
+	private function stubSwiftmailer()
+	{
+		return include(__DIR__ . '/stubs/swiftmailerHit.php');
 	}
 
 	public function test_it_returns_title()
 	{
-		$array = $this->stubHit();
+		$array = $this->stubPlain();
 		$array['_source']['title'] = 'Great Title Folks!';
 
 		$hit = new Hit($array);
@@ -36,7 +31,7 @@ class HitTest extends TestCase {
 
 	public function test_it_returns_url()
 	{
-		$array = $this->stubHit();
+		$array = $this->stubPlain();
 		$array['_source']['slug'] = 'really-fantastic-no-really';
 
 		$hit = new Hit($array);
@@ -46,14 +41,14 @@ class HitTest extends TestCase {
 
 	public function test_it_correctly_tests_if_highlight_returned()
 	{
-		$hit = new Hit($this->stubHit());
+		$hit = new Hit($this->stubPlain());
 
 		$this->assertTrue($hit->hasHighlight());
 	}
 
 	public function test_it_correctly_tests_if_highlight_not_returned()
 	{
-		$array = $this->stubHit();
+		$array = $this->stubPlain();
 		unset($array['highlight']);
 
 		$hit = new Hit($array);
@@ -70,8 +65,13 @@ class HitTest extends TestCase {
 
 	public function test_it_does_not_add_ellipsis_if_fragment_is_not_beginning_of_body()
 	{
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
+		$array = $this->stubSwiftmailer();
+
+		$hit = new Hit($array);
+
+		$this->assertEquals(
+			'Configuration<span class=\'search-line-break\'>|</span>Laravel provides a clean, simple API over the popular <mark>SwiftMailer</mark> library. The mail configuration file<span class="search-ellipsis">...</span>',
+			$hit->fragments()
 		);
 	}
 
@@ -84,18 +84,7 @@ class HitTest extends TestCase {
 
 	public function test_it_does_not_add_ellipsis_if_fragment_is_end_of_body()
 	{
-		$array = $this->stubHit();
-		// @todo extract
-		$array['_source']['body.highlighting'] = <<<EOF
-Mail & Local Development
-
-When developing an application that sends e-mail, it's usually desirable to disable the sending of messages from your local or development environment. To do so, you may either call the Mail::pretend method, or set the pretend option in the config/mail.php configuration file to true. When the mailer is in pretend mode, messages will be written to your application's log files instead of being sent to the recipient.
-
-If you would like to actually view the test e-mails, consider using a service like MailTrap.
-EOF;
-		$array['highlight']['body.highlighting'] = [' files instead of being sent to the recipient.
-
-If you would like to actually view the test e-mails, consider using a service like <mark>MailTrap</mark>.'];
+		$array = $this->stubMailtrap();
 
 		$hit = new Hit($array);
 
---
+++ tests/stubs/plainHit.php
@@ -0,0 +1,17 @@
+<?php
+return [
+	'_id' => '1234512351235',
+	'_score' => 5,
+	'_source' => [
+		'title' => null,
+		'slug' => null,
+		'body.md' => null,
+		'body.highlighting' => null,
+		'body.html' => null
+	],
+	'highlight' => [
+		'body.highlighting' => [
+			'fragment <mark>here</mark> I think'
+		]
+	]
+];
